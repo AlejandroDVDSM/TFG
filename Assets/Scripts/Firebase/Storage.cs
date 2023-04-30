14d88838e9330c8f103bafa74711a55872867e51
AlejandroDVDSM <bysalexx@gmail.com>
2023-04-30 13:04:54 +0100

    Remove test lines of code

--- Assets/Scripts/Firebase/Storage.cs
+++ Assets/Scripts/Firebase/Storage.cs
@@ -10,9 +10,8 @@ public class Storage : MonoBehaviour
     private Storage _instance;
     private bool _initialized;
     public bool Initialized => _initialized;
-    private string _baseURL;
-    private string _versionURL;
-    private int _latestVersion = 3;
+    private int _latestVersion = 1;
+    private string storageBucket;
     
     public static event Action OnStorageInitialized;
 
@@ -23,12 +22,16 @@ public class Storage : MonoBehaviour
             _instance = this;
         else
             Destroy(gameObject);
-    }    
+    }
     
     void Start()
     {
         if (!Initialized)
             FirebaseInitializer.OnDependenciesFixed += InitializeStorage;
+        
+        storageBucket = FindObjectOfType<JSONHelper>().GetValueFromJson("google-services", "$.project_info.storage_bucket");
+        if (PlayerPrefs.HasKey("version"))
+            _latestVersion = PlayerPrefs.GetInt("version");
     }
 
     private void OnDestroy()
@@ -40,30 +43,12 @@ public class Storage : MonoBehaviour
     {
         Debug.Log("Initializing Firebase Storage...");
         _storage = FirebaseStorage.DefaultInstance;
-        DefineBaseUrl();
         _initialized = true;
         OnStorageInitialized?.Invoke();
     }
-
-    private void DefineBaseUrl()
-    {
-        string storageBucket = FindObjectOfType<JSONHelper>()
-            .GetValueFromJson("google-services", "$.project_info.storage_bucket");
-
-
-        if (PlayerPrefs.HasKey("version"))
-            _latestVersion = 2;/*PlayerPrefs.GetInt("version");*/ // TEST TEST TEST TEST
-
-        //_versionURL = $"Versions/{/*version*/2}";
-        _baseURL = $"gs://{storageBucket}/Versions/{_latestVersion}";// TEST TEST TEST TEST// TEST TEST TEST TEST
-        Debug.Log($"Set BaseUrl: '{_baseURL}'");
-    }
     
-    private string GetVersionURL(int version)
+    private string GetUrl(int version)
     {
-        string storageBucket = FindObjectOfType<JSONHelper>()
-            .GetValueFromJson("google-services", "$.project_info.storage_bucket");
-
         return $"gs://{storageBucket}/Versions/{version}";
     }
     
@@ -83,15 +68,14 @@ public class Storage : MonoBehaviour
         }
     }
 
-    
     private void GetSpriteFromStorage(string storagePath, ITarget target, int version)
     {
-        StorageReference pathReference = _storage.GetReferenceFromUrl($"{GetVersionURL(version)}/{storagePath}");
+        StorageReference pathReference = _storage.GetReferenceFromUrl($"{GetUrl(version)}/{storagePath}");
         pathReference.GetDownloadUrlAsync().ContinueWithOnMainThread(task =>
         {
             if (task.IsCanceled || task.IsFaulted)
             {
-                Debug.LogError($"Error getting '{pathReference}' from Storage. Exception: '{task.Exception}'");
+                Debug.LogWarning($"Error getting '{pathReference}' from Storage. Exception: '{task.Exception}'");
                 RetryInitializeSprite(storagePath, target, version - 1);
             }
             else
@@ -99,7 +83,7 @@ public class Storage : MonoBehaviour
                 string localPath = $"{Application.persistentDataPath}/Versions/{version}/{storagePath}";
                 StartCoroutine(SpriteLoader.GetInstance().DownloadSprite(task.Result.ToString(), localPath, target));
             }
-        });        
+        });
     }
 
     private void RetryInitializeSprite(string storagePath, ITarget target, int version)
@@ -120,5 +104,5 @@ public class Storage : MonoBehaviour
             Debug.Log($"Didn't find local sprite with older version in '{localPath}'. Preparing things to get it from Firebase Storage");
             GetSpriteFromStorage(storagePath, target, version);
         }
-    }    
+    }
 }
